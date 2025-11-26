cf0cc83757b987b619db23a0a2569436c583779a
KyriakosKarda <kyriakoskardampikis2005@gmail.com>
2025-11-26 20:13:41 +0200

    Basic Infra

---
+++ src/test/java/SFG/Street_Food_Go/StreetFoodGoApplicationTests.java
@@ -0,0 +1,13 @@
+package SFG.Street_Food_Go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetFoodGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
