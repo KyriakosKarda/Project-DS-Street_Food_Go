d1a8ce08b75fb7d7d3c2ccaf12cc5d56dc8157d5
KyriakosKarda <kyriakoskardampikis2005@gmail.com>
2025-11-26 21:13:26 +0200

    New UI

---
+++ src/main/java/SFG/Street_Food_Go/Controllers/TestController.java
@@ -0,0 +1,13 @@
+package SFG.Street_Food_Go.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "homepage";
+    }
+}
